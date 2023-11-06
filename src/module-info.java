4705017a95ee7bee25d9098575593e736a2cbe81
Dionysis Theodosis <theo_187@hotmail.com>
2023-11-06 10:16:50 +0200

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module trustBox {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
-	requires java.desktop;
-	requires javafx.swing;
-	opens application to javafx.graphics, javafx.fxml;
-}
